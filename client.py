e4b9954abe42c2c9943ea3193cafb2361cef1854
Nacho Fregeiro <nachofregeiro@gmail.com>
2021-09-16 10:52:36 -0300

    Update param names and order

--- client.py
+++ client.py
@@ -29,38 +29,38 @@ def get_auth_token(auth_info):
     return get_response_field(response, auth_info["token_field"])
 
 
-def post_data(endpoint, data, headers={}, retry_get_auth_token=True):
+def post_data(auth_info, url, data, headers={}, retry_get_auth_token=True):
     print("post_data")
     global token
     if token == None:
         token = get_auth_token(auth_info)
     headers["Authorization"] = 'Bearer {}'.format(token)
-    response = requests.post(endpoint, data=data, headers=headers)
+    response = requests.post(url, data=data, headers=headers)
 
     if response.status_code == 401 and retry_get_auth_token:
         # token expired, refresh token and retry
         print("token expired")
         token = None
-        return post_data(endpoint, auth_info, params=params, headers=headers, retry_get_auth_token=False)
+        return post_data(auth_info, url, params=params, headers=headers, retry_get_auth_token=False)
 
     if response.status_code != 200:
         print(response)
     
     return response
 
-def get_data(endpoint, auth_info, params={}, headers={}, retry_get_auth_token=True):
+def get_data(auth_info, url, params={}, headers={}, retry_get_auth_token=True):
     print("get_data")
     global token
     if token == None:
         token = get_auth_token(auth_info)
     headers["Authorization"] = 'Bearer {}'.format(token)
-    response = requests.get(endpoint, params=params, headers=headers)        
+    response = requests.get(url, params=params, headers=headers)        
     
     if response.status_code == 401 and retry_get_auth_token:
         # token expired, refresh token and retry
         print("token expired")
         token = None
-        return get_data(endpoint, auth_info, params=params, headers=headers, retry_get_auth_token=False)
+        return get_data(auth_info, url, params=params, headers=headers, retry_get_auth_token=False)
 
     if response.status_code != 200:
         print(response)
@@ -68,13 +68,13 @@ def get_data(endpoint, auth_info, params={}, headers={}, retry_get_auth_token=Tr
     return response
 
 
-def get_paged_results(url, auth_info, page_param_name, page=1, data_field=None, params={}, headers={}):
+def get_paged_results(auth_info, url, page_param_name, page=1, data_field=None, params={}, headers={}):
     results = []
 
     while True:
         params[page_param_name] = page
         print(params)
-        response = get_data(url, auth_info, params=params, headers=headers)
+        response = get_data(auth_info, url, params=params, headers=headers)
         data = get_response_field(response, data_field)
         results = results + data
         page = page + 1
@@ -86,7 +86,7 @@ def get_paged_results(url, auth_info, page_param_name, page=1, data_field=None, 
 
     return results
 
-def get_paged_results_by_date(url, auth_info, date_from_param_name, date_to_param_name, date_from, date_to, minutes_span=10, data_field=None, params={}, headers={}):
+def get_paged_results_by_date(auth_info, url, date_from_param_name, date_to_param_name, date_from, date_to, minutes_span=10, data_field=None, params={}, headers={}):
     results = []
     time_delta = datetime.timedelta(minutes=minutes_span)
     partial_date_to = date_from
@@ -100,7 +100,7 @@ def get_paged_results_by_date(url, auth_info, date_from_param_name, date_to_para
         params[date_to_param_name] = partial_date_to.strftime("%m/%d/%Y, %H:%M:%S")
         print(params)
 
-        response = get_data(url, auth_info, params=params, headers=headers)
+        response = get_data(auth_info, url, params=params, headers=headers)
         data = get_response_field(response, data_field)
         results = results + data
         
@@ -129,7 +129,7 @@ if __name__=="__main__":
         "pageSize": 10
     }
 
-    response = get_paged_results("http://localhost:4000/users", auth_info, "page", params=params)
+    response = get_paged_results(auth_info, "http://localhost:4000/users", "page", params=params)
     
     print(response)
 
@@ -139,6 +139,6 @@ if __name__=="__main__":
     date_from = today - delta 
     date_to = today
 
-    response = get_paged_results_by_date("http://localhost:4000/users", auth_info, "from", "to", date_from, date_to, minutes_span=20)
+    response = get_paged_results_by_date(auth_info, "http://localhost:4000/users", "from", "to", date_from, date_to, minutes_span=20)
 
     print(response)        
